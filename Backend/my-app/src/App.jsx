064c6c4e8b1d5c7fe15a9c659922b5c9cb8f92bc
Geneticsquash <calle-daag@hotmail.com>
2025-10-01 13:10:54 +0200

    Created a vite project, add Recipe API by fetch

---
+++ Backend/my-app/src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const [newRecipe, setNewRecipe] = useState({
+    title: "",
+    description: "",
+    imageUrl: "",
+    timeInMins: "",
+    price: "",
+    ingredients: [{ name: "", amount: "", unit: "" }],
+  });
+
+  useEffect(() => {
+    fetch("https://grupp2-vtsor.reky.se/recipes")
+      .then((res) => {
+        if (!res.ok) throw new Error("Network response was not ok");
+        return res.json();
+      })
+      .then((data) => {
+        setRecipes(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Fetch error:", err);
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Receptsajt
+
+We are student at ITHS doing a collab work. 
+
+This project is a REST API for recipes.
+
+You can:
+
+Get, Search, Add, Update and Delete.
