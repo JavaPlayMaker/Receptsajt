01b011a6603ed0709e79678a3460f3388434ca81
david rydman <david.rydman@iths.se>
2025-10-07 22:58:06 +0200

    add text to Header

---
+++ Backend/my-app/src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header>
+      <h1>SWEET HEADER!</h1>
+      <nav>
+        <ul>
+          <li>Rice n Roll</li>
+          <li>Pasta</li>
+          <li>Kyckling</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
