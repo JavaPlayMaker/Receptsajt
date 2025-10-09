e1ec788a288d52d80ad3a4271388a4369b99a97e
moaherback <moa.herback@gmail.com>
2025-10-09 19:37:38 +0200

    created link from homepage to recipe page and added logo to assets

--- Backend/my-app/src/pages/Home.jsx
+++ Backend/my-app/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import SearchBar from "../components/SearchBar";
+import { Link } from "react-router";
 
 const Home = () => {
   const [recipes, setRecipes] = useState([]);
@@ -8,23 +9,27 @@ const Home = () => {
   const [filteredRecipes, setFilteredRecipes] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
 
-
   useEffect(() => {
     fetch("https://grupp2-vtsor.reky.se/recipes")
       .then((res) => {
         if (!res.ok) throw new Error("Network response was not ok");
         return res.json();
       })
-      .then((data) => {setRecipes(data); setFilteredRecipes(data);})
+      .then((data) => {
+        setRecipes(data);
+        setFilteredRecipes(data);
+      })
       .catch((err) => setError(err.message))
       .finally(() => setLoading(false));
   }, []);
 
   const handleSearch = () => {
-  const query = searchQuery.toLowerCase();
-  const results = recipes.filter((r) => r.title.toLowerCase().includes(query));
-  setFilteredRecipes(results);
-};
+    const query = searchQuery.toLowerCase();
+    const results = recipes.filter((r) =>
+      r.title.toLowerCase().includes(query)
+    );
+    setFilteredRecipes(results);
+  };
 
   if (loading) return <p> Loading recipes.</p>;
   if (error) return <p>Error: {error}</p>;
@@ -32,36 +37,39 @@ const Home = () => {
   return (
     <div>
       <h1>Recipes</h1>
-      <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} onSearch={handleSearch}/>
-      
-      
-      
+
+      <SearchBar
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        onSearch={handleSearch}
+      />
+
       {filteredRecipes.length > 0 ? (
-                  <ul>
-                      {filteredRecipes.map((r) => (
-                          <li key={r.id}>
-                              <h2>{r.title}</h2>
-                              {r.imageUrl && <img src={r.imageUrl} alt={r.title} width="150" />}
-                              <p>{r.description}</p>
-                              <p>
-                                  Time: {r.timeInMins} mins |  Price: {r.price} SEK
-                              </p> 
-                              <ul>
-          {r.ingredients.map((ing, i ) => (
-              <li key={i}>
-      {ing.amount} {ing.unit} {ing.name}
-          </li>
-                    ))}
-                  </ul>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>No recipes found.</p>
-          )}
-        </div>
-      );
+        <ul>
+          {filteredRecipes.map((r) => (
+            <li key={r._id}>
+               <Link to={`/recipe/${r._id}`}>
+              <h2>{r.title}</h2>
+              {r.imageUrl && <img src={r.imageUrl} alt={r.title} width="150" />}
+              <p>{r.description}</p>
+              <p>
+                Time: {r.timeInMins} mins | Price: {r.price} SEK
+              </p>
+              </Link>
+              <ul>
+                {r.ingredients.map((ing, i) => (
+                  <li key={i}>
+                    {ing.amount} {ing.unit} {ing.name}
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No recipes found.</p>
+      )}
+    </div>
+  );
 };
 export default Home;
-
-
