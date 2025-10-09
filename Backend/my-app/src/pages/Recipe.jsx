7636d520e3b432fcf7b60ff566edfad7ff266820
moaherback <moa.herback@gmail.com>
2025-10-09 20:01:45 +0200

    add RatingStars to recipe page

--- Backend/my-app/src/pages/Recipe.jsx
+++ Backend/my-app/src/pages/Recipe.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import RatingStars from "../components/StarRating";
 
 const Recipe = () => {
     const { id } = useParams();
@@ -38,6 +39,7 @@ const Recipe = () => {
                   </li>
                 ))}
               </ul>
+              <RatingStars recipeId={recipe._id}/>
      </>
     ) : (
       <p>No recipe found.</p>
