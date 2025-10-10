9cdcef11b173f05c90f6cc9bd458e8bb91ed44f9
moaherback <moa.herback@gmail.com>
2025-10-10 09:42:05 +0200

    move the recipe page fetch to api.js

--- Backend/my-app/src/services/api.js
+++ Backend/my-app/src/services/api.js
@@ -7,7 +7,7 @@ export async function postRating(recipeId, rating) {
     body: JSON.stringify({ rating }),
   });
   if (!res.ok) throw new Error("Failed to submit rating");
-  return null; // change the return to null from res.json. respons have no j.son so the app was catching error eve tho the API worked 
+  return null; // change the return to null from res.json. respons have no j.son so the app was catching error eve tho the API worked
 }
 
 export async function getComments(recipeId) {
@@ -23,5 +23,11 @@ export async function postComment(recipeId, name, text) {
     body: JSON.stringify({ name, text }),
   });
   if (!res.ok) throw new Error("Failed to submit comment");
+  return res.json();
+}
+
+export async function getRecipe(recipeId) { // added my fetch here, do we want all the fetch in api.js? 
+  const res = await fetch(`${BASE_URL}/recipes/${recipeId}`);
+  if (!res.ok) throw new Error("Network response was not ok");
   return res.json();
 }
