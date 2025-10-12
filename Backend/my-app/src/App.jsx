0cfb7ca676fb9d696d3675e69986814b33402f8e
david rydman <david.rydman@iths.se>
2025-10-12 21:03:10 +0200

    Adds RootLayout 'Route' in App.jsx

--- Backend/my-app/src/App.jsx
+++ Backend/my-app/src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Recipe from "./pages/Recipe";
+import RootLayout from "./rootLayout/RootLayout";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/recipe/:id" element={<Recipe />} />
+      <Route path="/" element={<RootLayout />} >
+        <Route path="/" element={<Home />} />
+        <Route path="/recipe/:id" element={<Recipe />} />
+      </Route>
     </Routes>
   );
 }
