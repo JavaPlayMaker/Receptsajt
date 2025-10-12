63c708cc4958f4557af0c706da6fc683c81c1f52
david rydman <david.rydman@iths.se>
2025-10-12 21:29:54 +0200

    Deletes a double of RootLayout and corrects the import of RootLayout.jsx

--- Backend/my-app/src/App.jsx
+++ Backend/my-app/src/App.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Recipe from "./pages/Recipe";
-import RootLayout from "./rootLayout/RootLayout";
+import RootLayout from "./layout/RootLayout";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<RootLayout />} >
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} /> {/* TODO Sätta en annan path? */}
         <Route path="/recipe/:id" element={<Recipe />} />
       </Route>
     </Routes>
