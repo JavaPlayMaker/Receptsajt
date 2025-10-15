2703287af48805ad110ebd693a5ce46e7e36940f
david rydman <david.rydman@iths.se>
2025-10-15 09:05:42 +0200

    Adds links to Navbar

---
+++ Backend/my-app/src/layout/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Navbar.css";
+import SearchBar from "../components/SearchBar";
+import logo from "../assets/RICE-N-ROLL.png";
+import { Link } from "react-router";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src={logo} alt="RICE N ROLL" className="header-logo" />
+
+      <ul className="nav-ul">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>Recipe</li>
+        <li>
+          <Link to="Category"></Link>Category
+        </li>{" "}
+        {/* TODO skapa undermeny! */}
+      </ul>
+
+      <div className="searchbar">
+        <SearchBar />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
