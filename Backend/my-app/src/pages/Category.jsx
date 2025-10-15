feab9fb7273af38dd13ccf609929eed3bb2db514
Geneticsquash <calle-daag@hotmail.com>
2025-10-15 09:50:58 +0200

    Added code from my laptop, coulnt push it, will add prevois commits later.

--- Backend/my-app/src/pages/Category.jsx
+++ Backend/my-app/src/pages/Category.jsx
@@ -2,13 +2,12 @@ import React, { useState, useEffect } from "react";
 import TodoList from "../components/ToDoList.jsx";
 
 const Category = () => {
-return (
-<div>
-<h1>Category Page</h1>
-<TodoList />
-</div>
-);
+    return (
+        <div>
+            <h1>Category Page</h1>
+            <TodoList />
+        </div>
+    );
 };
 
-export default Category;
-
+export default Category; 
