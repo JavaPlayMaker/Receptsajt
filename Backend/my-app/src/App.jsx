cca099b32538d62066d20944d87077081340b88b
david rydman <david.rydman@iths.se>
2025-10-22 23:46:22 +0200

    Refactor 'Category.jsx' to 'CategoryDropDown.jsx' and deletes 'Category.css'

--- Backend/my-app/src/App.jsx
+++ Backend/my-app/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Recipe from "./pages/Recipe";
 import RootLayout from "./layout/RootLayout";
-import Category from "./pages/Category";
+import CategoryDropDown from "./layout/CategoryDropDown";
 
 
 export function App() {
@@ -12,7 +12,7 @@ export function App() {
       <Route path="/" element={<RootLayout />} >
         <Route path="/" element={<Home />} />
         <Route path="/recipe/:id" element={<Recipe />} />
-        <Route path="/category/:category" element={<Category />} />
+        <Route path="/category/:category" element={<CategoryDropDown />} />
       </Route>
     </Routes>
   );
