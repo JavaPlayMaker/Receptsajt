1940adb85eeecfe29fab79b4516b63c6aac2cede
moaherback <moa.herback@gmail.com>
2025-10-22 16:16:20 +0200

    Add test on difficulty

--- Backend/my-app/vite.config.js
+++ Backend/my-app/vite.config.js
@@ -7,4 +7,9 @@ export default defineConfig({
     react(),
     tsconfigPaths()
   ],
+   test: {
+    globals: true,           
+    environment: "happy-dom",     
+    setupFiles: "./src/test/setupTests.js"
+  },
 });
