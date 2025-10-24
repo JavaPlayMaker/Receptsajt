c71a3e598d90f63555668cf767c1766a487bf5f9
david rydman <david.rydman@iths.se>
2025-10-24 16:09:08 +0200

    Fix layout of CategoryDropDown

--- Backend/my-app/src/components/CategoryDropDown.jsx
+++ Backend/my-app/src/components/CategoryDropDown.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { getRecipesByCategory } from "../services/api";
-import "../pages/Home.css";
+// import "../pages/Home.css";
+import "./CategoryDropDown.css"
 
 export default function CategoryDropDown() {
   const { category } = useParams();
